336cdd39163bc54931d283360654490ec4417b86
ounzin <ahmedadjibade9@gmail.com>
2021-04-03 10:58:48 +0200

    filelist mod 2

--- filelist.py
+++ filelist.py
@@ -3,33 +3,42 @@
 import os, os.path,sys,subprocess
 from options import *
 
-def ls():
-    a = subprocess.run(['ls'],capture_output=True, text=True).stdout
+def ls(a):
+    ls_out = []
+    a = subprocess.run(['ls',a],capture_output=True, text=True).stdout
     b = a.split('\n')
-    return b
+    for i in range(len(b)):
+        if b[i] != '':
+            ls_out.append(b[i])
+    return ls_out
 
 def lister(addr_source):
     filelist = []
     if os.path.isfile(addr_source):
         a = os.path.basename(addr_source)
         filelist.append(a)
+ 
     elif os.path.islink(addr_source):
         print("link")
+
     elif os.path.isdir(addr_source):
         inner_dir = []
         pid = os.fork()
         if pid == 0:
-            inner_dir = ls()
+            inner_dir = ls(addr_source)
             #parent_dir = os.getcwd()
             for i in range(len(inner_dir)):
                 inner_path = os.path.join(addr_source,inner_dir[i])
-                print(inner_path)
-                #lister(inner_path)
+                a = lister(inner_path)
+                filelist.append(a)
             sys.exit(0)
         else:
             pid_wait, status = os.waitpid(-1,0)
     else:
         pass
     return filelist
-
-print(lister(SRC))
+    
+A = lister(SRC)
+print(A)
+#lister(SRC)
+#print(ls(SRC))
